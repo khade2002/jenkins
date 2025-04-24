1ae4c42a8fcad2ac7358a32dcaef3adead012b37
khade1676@gmail.com <khade1676@gmail.com>
2025-04-24 20:35:07 +0000

    commiting

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
 public class myfile {
     public static void main(String[] args) {
-        System.out.println("Hello World from jenkins !");
+        System.out.println("try jenkins !");
     }
 }
